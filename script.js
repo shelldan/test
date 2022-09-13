f6d6cdc7f8f9ecb7fc3ff72d70b2921579c2a6ec
shelldan <shelldanlee@gmail.com>
2022-09-13 17:28:23 -0500

    video

--- script.js
+++ script.js
@@ -1,147 +1,29 @@
-//Erica's Task - creating .fetch to spoonacular API
+var API_KEY = 'AIzaSyCPVbJouFqqk56R4EteKzKMhY703BMSE_M'
+var cuisine = 'Ratatouille Pasta'
 
-//Spoonacular API documentation: https://spoonacular.com/food-api/docs
-//Spoonacular Authentication: https://spoonacular.com/food-api/docs#Authentication
-var page2Div = document.createElement('div')
-var page3Div = document.createElement('div')
-var page4Div = document.createElement('div')
+//maxResults = 1, returning 1 result(video)
+var url = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q='+ cuisine + '&key=' + API_KEY
 
-var page1Div = document.querySelector('.page-1')
-
-var cuisineBtn = document.getElementById('cuisine')
-var mainDiv = document.querySelector('main')
-
-var frenchStyle = document.getElementById('french')
-var spoonApiKey = 'c95dcc6d89ab47a384f18b2f989d7286'
-
-var ids = [] //might need to use array to remove duplicate id
-//var recipeInfoUrl = 'https://api.spoonacular.com/recipes/' + id +'/information'
+// fetch(url)
+// .then(function(response){
+//     return response.json()
+// })
+// .then(function(data){
+//     console.log(data)
+//     console.log(data.items[0].id.videoId) //return youtubeVideoId
 
 
 
-function page2handler (event){
-
-    page1Div.style.display = 'none'
-
-    page2Div.setAttribute('class','page-2')
-
-    var french = document.createElement('button')
-    var american = document.createElement('button')
-    var german = document.createElement('button')
-    var italian = document.createElement('button')
-
-    mainDiv.appendChild(page2Div)
-    page2Div.appendChild(french)
-    page2Div.appendChild(american)
-    page2Div.appendChild(german)
-    page2Div.appendChild(italian)
-
-    french.innerHTML = 'French'
-    american.innerHTML = 'American'
-    german.textContent = 'German'
-    italian.textContent = 'Italian'
-
-    console.log(event.target.textContent) //cuisine
-
-    $("button").click(page3handler) //stackoverflow: how to get ID of button user just clicked? 
-    //$("button").click(getRecipeName)
-}
 
 
-function page3handler(event){
-
-    page2Div.style.display = 'none';
-
-    page3Div.setAttribute('class','page-3')
+//search stackoverflow: Embedding a youtube video on page using javascript 
 
 
-    console.log(event.target.textContent) // French 
-
-    //cuisineHeader.textContent = 'Bake Ratatouille' //for testing, will delete later 
-
-    var cuisine = event.target.textContent
-    
-    var cuisineUrl = 'https://api.spoonacular.com/recipes/complexSearch?' +'&cuisine=' + cuisine + '&apiKey=' + spoonApiKey
-    
-    console.log(cuisineUrl)
-
-    function getRecipeName(){
-        fetch(cuisineUrl)
-        .then(function(response){
-            return response.json()
-        })
-        .then(function(recipeName){
-            console.log(recipeName)
-            
-
-            for (var i=0; i<10; i++){ //limited to 10 per page, butwhy can't use i<recipeName.length? 
-                var cuisineHeader = document.createElement('h2')
-                var cuisineImg = document.createElement('img')
-                var cuisineRecipe = document.createElement('p') // not sure if we want to add here
-
-                cuisineHeader.textContent = recipeName.results[i].title
-                cuisineImg.src=recipeName.results[i].image
-                cuisineHeader.setAttribute('id',recipeName.results[i].id)
-
-                cuisineHeader.addEventListener('click',function(){
-                    console.log(this)
-                    var id = this.getAttribute('id')
-                    console.log(id)
-                    page4handler()
-                })
-
-                mainDiv.appendChild(page3Div)
-                page3Div.appendChild(cuisineHeader)
-                page3Div.appendChild(cuisineImg)
-                page3Div.appendChild(cuisineRecipe)
-
-            }
-
-           // cuisineHeader.addEventListener('click',page4handler)
-            
-            
-        })
+var videoId = 'Vbmr2Qr_1Ok'
+var obj ={
+    "video":{
+        "videoId":'Vbmr2Qr_1Ok',
+        "value":"<iframe title='YouTube video player' type=\'text/html\' width='640' height='390' src='http://www.youtube.com/embed/' " + this.videoId + "frameborder='0' allowFullScreen></iframe>"
     }
-    getRecipeName()
 }
-
-
-function page4handler(){
-    page2Div.style.display= 'none';
-    page3Div.style.display= 'none';
-
-    page4Div.setAttribute('class','page-4')
-
-    var cuisineHeader = document.createElement('h2')
-    var cuisineImg = document.createElement('img')
-    var cuisineRecipe = document.createElement('p')
-
-    mainDiv.appendChild(page4Div)
-    page4Div.appendChild(cuisineHeader)
-    page4Div.appendChild(cuisineImg)
-    page4Div.appendChild(cuisineRecipe)
-
-    var id = localStorage.getItem('id')
-
-    var recipeInfoUrl = 'https://api.spoonacular.com/recipes/' + id +'/information?' + '&apiKey=' + spoonApiKey
-
-    fetch(recipeInfoUrl)
-    .then(function(response){
-        return response.json()
-    })
-    .then(function(recipeInfo){
-        //console.log(recipeInfo)
-        cuisineHeader.textContent = recipeInfo.title
-        cuisineImg.src=recipeInfo.image
-        cuisineRecipe.innerHTML=recipeInfo.summary
-    })
-
-}
-
-
-cuisineBtn.addEventListener('click',page2handler)
-
-
-
-
-
+document.write(obj.video.value)
