3db4145e0dcf3dffc4bd7b18889f6e46d78d4a40
shelldan <shelldanlee@gmail.com>
2022-09-28 17:43:44 -0500

    update

--- script.js
+++ script.js
@@ -1,29 +1,14 @@
-var API_KEY = 'AIzaSyCPVbJouFqqk56R4EteKzKMhY703BMSE_M'
-var cuisine = 'Ratatouille Pasta'
-
-//maxResults = 1, returning 1 result(video)
-var url = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q='+ cuisine + '&key=' + API_KEY
-
-// fetch(url)
-// .then(function(response){
-//     return response.json()
-// })
-// .then(function(data){
-//     console.log(data)
-//     console.log(data.items[0].id.videoId) //return youtubeVideoId
-
-
-
-
-
-//search stackoverflow: Embedding a youtube video on page using javascript 
-
-
-var videoId = 'Vbmr2Qr_1Ok'
-var obj ={
-    "video":{
-        "videoId":'Vbmr2Qr_1Ok',
-        "value":"<iframe title='YouTube video player' type=\'text/html\' width='640' height='390' src='http://www.youtube.com/embed/' " + this.videoId + "frameborder='0' allowFullScreen></iframe>"
-    }
+const student = {
+  name: 'Alex',
+  age: 20,
+  email: 'alex@email.com'
 }
-document.write(obj.video.value)
+
+const studentStr = JSON.stringify(student)
+
+console.log(studentStr)
+console.log('I made it here')
+
+const jsObject = JSON.parse(studentStr)
+
+console.log(jsObject)
